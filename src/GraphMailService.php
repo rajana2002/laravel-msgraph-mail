e0dd74679d747a14184dd926dc81a5f6b5043c5a
Carsten <kontakt@openwebsolutions.de>
2025-09-16 11:41:09 +0200

    deleted old code

--- src/GraphMailService.php
+++ src/GraphMailService.php
@@ -5,23 +5,6 @@ namespace Mail\MsGraphMail;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
-class MsGraphServiceProvider extends ServiceProvider
-{
-    public function register(): void
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/msgraph.php', 'msgraph');
-        $this->app->singleton(GraphMailService::class, fn () => new GraphMailService());
-        $this->app->alias(GraphMailService::class, 'msgraph.mailer');
-    }
-
-    public function boot(): void
-    {
-        $this->publishes([
-            __DIR__.'/../config/msgraph.php' => config_path('msgraph.php'),
-        ], 'msgraph-config');
-    }
-}
-
 class GraphMailService
 {
     protected string $tenantId;
